8380b1522e586e4074fbb9679b5a1910d0db8990
kushagra-value <kushagra@valuebound.com>
2024-08-20 10:36:34 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,8 @@
+# tool.py
 from crewai_tools import YoutubeChannelSearchTool
 
 # Initialize the tool with a specific Youtube channel handle to target your search
 yt_tool = YoutubeChannelSearchTool(youtube_channel_handle='@krishnaik06')
 
+# Update this code based on your needs if ChromaDB is not required
+# or find an alternative that fits your use case
